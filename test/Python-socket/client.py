d77687e25448257168a9916dc19f5bf18cfe090a
Antiocher <antiocher36@gmail.com>
2025-07-19 22:17:30 +0300

    Update client.py

--- test/Python-socket/client.py
+++ test/Python-socket/client.py
@@ -1,1 +1,16 @@
+import socket
 
+host = (socket.gethostname(), 34567)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(host)
+
+s.listen()
+print('yeah')
+
+while True:
+	conn, addr = s.accept()
+	print('wtf', addr)
+	res = b'hello bro'
+	conn.send(res)
